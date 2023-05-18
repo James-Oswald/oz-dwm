06356e6d324f2c6649f016266e89367ec6f941b6
James Oswald <jamesoswald111@gmail.com>
2023-05-17 21:39:34 -0400

    Gaps patched

--- src/config.h
+++ src/config.h
@@ -107,7 +107,6 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,			            XK_Return, spawn,          {.v = termcmd } },
-	//Mod + b for "Browse"
 	{ MODKEY,             			XK_b, 	   spawn,          {.v = chromiumCmd } },
 	{ MODKEY,             			XK_c, 	   spawn,          {.v = vsCodeCmd } },
 	{ MODKEY, 						XK_s,	   spawn,		   {.v = screenShotCmd } },
@@ -144,14 +143,11 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|Mod1Mask,              XK_h,      incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod1Mask,              XK_l,      incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod1Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
-	{ MODKEY|Mod1Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ MODKEY|ShiftMask,             XK_h,      incrgaps,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_l,      incrgaps,       {.i = -1 } },
+	{ MODKEY,             			XK_g,      togglegaps,     {0} },
+	{ MODKEY|ShiftMask,			    XK_g,      defaultgaps,    {0} },
+/*
 	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
 	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
@@ -160,6 +156,7 @@ static const Key keys[] = {
 	{ MODKEY|Mod1Mask,              XK_o,      incrohgaps,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
+	*/
 };
 
 /* button definitions */
